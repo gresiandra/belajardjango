b7034b1c43957d650b559f4d6772d8c83450e73a
gresiandra <gresiandra@gmail.com>
2020-05-17 12:55:42 +0700

    'update_belajar_django'

--- belajar_djangodb/sosmed/urls.py
+++ belajar_djangodb/sosmed/urls.py
@@ -5,5 +5,6 @@ from .import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^create/$', views.create, name='create'),
-    url(r'^delete/(?P<delete_id>[0-9])$', views.delete, name='delete')
+    url(r'^delete/(?P<delete_id>[1-9]|[1-9][0-9]|[1-9][0-9][0-9])$', views.delete, name='delete'),
+    url(r'^update/(?P<update_id>[1-9]|[1-9][0-9]|[1-9][0-9][0-9])$', views.update, name='update'),
 ]
--- belajar_djangodb/sosmed/views.py
+++ belajar_djangodb/sosmed/views.py
@@ -36,3 +36,28 @@ def create(request):
 def delete(request, delete_id):
     sosmedModel.objects.filter(id=delete_id).delete()
     return redirect('sosmed:index')
+
+def update(request, update_id):
+
+    modelUpdate = sosmedModel.objects.get(id=update_id)
+
+    data = {
+        'nama_depan': modelUpdate.nama_depan,
+        'nama_belakang': modelUpdate.nama_belakang,
+        'username': modelUpdate.username,
+    }
+
+    form_update = sosmedForm(request.POST or None, initial=data, instance=modelUpdate)
+
+    if request.method == 'POST':
+        if form_update.is_valid():
+            form_update.save()
+
+        return redirect('sosmed:index')
+    
+    context = {
+        'heading':'Create User Sosmed',
+        'form_sosmed':form_update
+    }
+
+    return render(request, 'sosmed/create.html', context)

1e25f6e4b589c2c49d37eee225307d609cfb621e
gresiandra <gresiandra@gmail.com>
2020-05-18 09:55:35 +0700

    'classBased2'

--- belajar_djangodb/classBased/urls.py
+++ belajar_djangodb/classBased/urls.py
@@ -1,7 +1,21 @@
 from django.conf.urls import url
+from django.views.generic.base import TemplateView
 
 from .import views
 
 urlpatterns = [
+
+    #Beberapa cara memanggil template halaman menggunakan ClassBasedView
+
+    #cara pertama : memakai class View saja
     url(r'^$', views.IndexClass.as_view(), name='index'),
+
+    #cara kedua : memakai class TemplateView yang di deklarasikan di view.py
+    url(r'^index1$', views.IndexClass2.as_view(template_name='classBased/classBased1.html'), name='index1'),
+
+    #cara ketiga : langsung pakai class TemplateView di urls.py (statis)
+    url(r'^index2$', TemplateView.as_view(template_name='classBased/classBased2.html'), name='index2'),
+
+    #cara keempat : mengambil context pada TemplateView (lihat di views.py)
+    url(r'^indexContext$', views.IndexContext.as_view(), name='indexContext')
 ]
--- belajar_djangodb/classBased/views.py
+++ belajar_djangodb/classBased/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
@@ -16,3 +17,18 @@ class IndexClass(View):
     def post(self, request):
         self.context['heading'] = 'ini POST'
         return render(request, self.template_name, self.context)
+
+
+class IndexClass2(TemplateView):
+    pass
+
+class IndexContext(TemplateView):
+    template_name = 'classBased/indexContext.html'
+
+    def get_context_data(self):
+        context = {
+            'heading':'CONTEXT',
+            'content': 'INI ISI CONTENT'
+        }
+
+        return context
